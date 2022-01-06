cf569fe802f7bd288b408a197012adcf1d5f67cb
bradcglabs <63960298+bradcglabs@users.noreply.github.com>
2022-01-06 11:54:00 +0200

    Updated required information for profile

--- src/app/components/profile/profile.service.ts
+++ src/app/components/profile/profile.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@angular/core';
 
 @Injectable()
-export  class UserProfile {
+export class UserProfile {
   user = {
     userName: 'Gregory Strydom',
-    userEmail: 'gregstrydom5@gmail.com'
+    userEmail: 'gregstrydom5@gmail.com',
+    facebookUrl: 'https://www.facebook.com',
+    twitterUrl: 'https://www.twitter.com',
+    instagramUrl: 'https://www.instagram.com',
+    linkedinUrl: 'https://www.linkedin.com'
   };
+
   constructor() {
   }
 
